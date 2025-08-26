53ba412420aaff8f18285222ce73d0be6ac364a1
Strait <bstrait@una.edu>
2025-08-26 13:46:05 -0500

    paths WIP

--- app.mjs
+++ app.mjs
@@ -1,9 +1,31 @@
 import express from 'express'
+import path from 'path'
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const path = require('path')
 
 const app = express()
+const PORT = process.env.PORT || 3000;
+const __filename = 
+
+
+//const path = require('path');
+
+app.use(express.static(__dirname + 'public'));
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Hello Express from Render.<a href="/bailee">bailee</a>')
+})
+//endpoints...middlewares...apis?
+// send an html file
+app.get('/bailee', (req, res) => {
+  //res.send('bailee <a href="/">home</a>')
+  res.sendFile('bailee.html');
 })
 
-app.listen(3000)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
